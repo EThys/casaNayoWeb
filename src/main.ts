a5611295f4d2c9e5a2c97ed6ff7021270936ea2f
Ethys <ethbergmuzola500@gmail.com>
2025-12-22 15:36:43 +0100

    chore(main): register new plugins and global configurations

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { MotionPlugin } from '@vueuse/motion'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import App from './App.vue'
 import router from '@/router'
+// Import fetch wrapper to filter API-related console logs
+import '@/utils/fetchWrapper'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(MotionPlugin)
 
 app.mount('#app')
